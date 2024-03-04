fa76ea42eee6f9462d87b6d308bfdcf785792682
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-03-04 11:08:12 +0530

    Added new Code on 04/03/2024 at 11:07 AM

--- src/JIRATest.java
+++ src/JIRATest.java
@@ -1,9 +1,86 @@
 
+import static io.restassured.RestAssured.given;
+
+import java.io.File;
+
+import org.testng.Assert;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
+
 public class JIRATest {
 
 	public static void main(String[] args) {
-		
-		
+
+		RestAssured.baseURI = "http://localhost:9090/";
+
+		// Log in Scenarios:
+		SessionFilter session = new SessionFilter();
+		String responce = given().relaxedHTTPSValidation().header("Content-Type", "application/json")
+				.body("{\r\n" + "    \"username\":\"Dibyansh.verma25\",\r\n" + "    \"password\":\"Dibyansh@1998\"\r\n"
+						+ "}")
+				.log().all().filter(session).when().post("/rest/auth/1/session").then().log().all().extract().response()
+				.asString();
+		System.out.println(responce);
+
+		String expectedMessage = "My Name is Dibyansh";
+		// Add comment Code
+		String addcommentresponse = given().pathParam("id", "10100").log().all()
+				.header("Content-Type", "application/json")
+				.body("{\r\n" + "    \"body\": \"" + expectedMessage + "\",\r\n" + "    \"visibility\": {\r\n"
+						+ "        \"type\": \"role\",\r\n" + "        \"value\": \"Administrators\"\r\n" + "    }\r\n"
+						+ "}")
+				.filter(session).when().post("/rest/api/2/issue/{id}/comment").then().log().all().assertThat()
+				.statusCode(201).extract().response().asString();
+		JsonPath js = new JsonPath(addcommentresponse);
+		String commentId = js.get("id");
+
+		// Add Attachment
+
+		given().header("X-Atlassian-Token", "no-check").filter(session).pathParam("key", "10100")
+
+				.header("Content-Type", "multipart/form-data")
+
+				.multiPart("file", new File("jira.txt")).when().
+
+				post("rest/api/2/issue/{key}/attachments").then().log().all().assertThat().statusCode(200);
+
+		// Get Issue
+
+		String issueDetails = given().filter(session).pathParam("key", "10100")
+
+				.queryParam("fields", "comment")
+
+				.log().all().when().get("/rest/api/2/issue/{key}").then()
+
+				.log().all().extract().response().asString();
+
+		System.out.println(issueDetails);
+
+		JsonPath js1 = new JsonPath(issueDetails);
+
+		int commentsCount = js1.getInt("fields.comment.comments.size()");
+
+		for (int i = 0; i < commentsCount; i++)
+
+		{
+
+			String commentIdIssue = js1.get("fields.comment.comments[" + i + "].id").toString();
+
+			if (commentIdIssue.equalsIgnoreCase(commentId))
+
+			{
+
+				String message = js1.get("fields.comment.comments[" + i + "].body").toString();
+
+				System.out.println(message);
+
+				Assert.assertEquals(message, expectedMessage);
+
+			}
+
+		}
 
 	}
 
